1b19832de543fda5fd06d7f359eeab4497c053d5
wen9xin <64579831+wen9xin@users.noreply.github.com>
2020-11-25 16:56:06 +0100

    TODO 2 解决 只剩下1.4B还是问题，先暂留，张召你能解决吗？ 应该是在创建view LesEuipes的时候的（CreatedDB.sql中）

--- actions/action_fct_comp_11.py
+++ actions/action_fct_comp_11.py
@@ -20,11 +20,15 @@ class AppFctComp11(QDialog):
     def refreshResult(self):
 
         display.refreshLabel(self.ui.label_fct_comp_11, "")
+
         try:
             cursor = self.data.cursor()
-            result = cursor.execute("SELECT numSP,nomSp, prenomSp, pays, categorieSp,dateNaisSp,ageSp FROM LesSportifs_base")
+            result = cursor.execute("SELECT numSP,nomSp, prenomSp, pays, categorieSp,dateNaisSp,ageSp FROM LesSportifs")
+
         except Exception as e:
             self.ui.table_fct_comp_11.setRowCount(0)
             display.refreshLabel(self.ui.label_fct_comp_11, "Impossible d'afficher les résultats : " + repr(e))
         else:
             display.refreshGenericData(self.ui.table_fct_comp_11, result)
+
+
