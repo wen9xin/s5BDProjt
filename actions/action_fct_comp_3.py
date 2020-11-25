d8271c88b193e2fbe18b8e5189aa6b0929f72af1
wen9xin <64579831+wen9xin@users.noreply.github.com>
2020-11-25 16:15:59 +0100

    1.6解决 1.4B还是问题，先暂留，张召你能解决吗？

--- actions/action_fct_comp_3.py
+++ actions/action_fct_comp_3.py
@@ -13,6 +13,7 @@ class AppFctComp3(QDialog):
         super(QDialog, self).__init__()
         self.ui = uic.loadUi("gui/fct_comp_3.ui", self)
         self.data = data
+        self.refreshCatList()
 
     # Fonction de mise à jour de l'affichage
     @pyqtSlot()
@@ -20,19 +21,28 @@ class AppFctComp3(QDialog):
         # TODO 1.6 : fonction à modifier pour remplacer la zone de saisie par une liste de valeurs issues de la BD une
         #  fois le fichier ui correspondant mis à jour
         display.refreshLabel(self.ui.label_fct_comp_3, "")
-        if not self.ui.lineEdit_fct_comp_3.text().strip():
-            self.ui.table_fct_comp_3.setRowCount(0)
-            display.refreshLabel(self.ui.label_fct_comp_3, "Veuillez indiquer un nom de catégorie")
+        try:
+           cursor = self.data.cursor()
+           result = cursor.execute(
+               "SELECT numEp, nomEp, formeEp, nomDi, nbSportifsEp, dateEp FROM LesEpreuves WHERE categorieEp = ?",
+               [self.ui.comboBox_comp_3.currentText()])
+        except Exception as e:
+               self.ui.table_fct_comp_3.setRowCount(0)
+               display.refreshLabel(self.ui.label_fct_comp_3, "Impossible d'afficher les résultats : " + repr(e))
         else:
-            try:
-                cursor = self.data.cursor()
-                result = cursor.execute(
-                    "SELECT numEp, nomEp, formeEp, nomDi, nbSportifsEp, dateEp FROM LesEpreuves WHERE categorieSp = ?",
-                    [self.ui.lineEdit_fct_comp_3.text().strip()])
-            except Exception as e:
-                self.ui.table_fct_comp_3.setRowCount(0)
-                display.refreshLabel(self.ui.label_fct_comp_3, "Impossible d'afficher les résultats : " + repr(e))
-            else:
-                i = display.refreshGenericData(self.ui.table_fct_comp_3, result)
-                if i == 0:
+               i = display.refreshGenericData(self.ui.table_fct_comp_3, result)
+               if i == 0:
                     display.refreshLabel(self.ui.label_fct_comp_3, "Aucun résultat")
+
+    # Fonction de mise à jour des catégories
+    @pyqtSlot()
+    def refreshCatList(self):
+
+        try:
+            cursor = self.data.cursor()
+            result = cursor.execute("SELECT DISTINCT categorieEp FROM LesEpreuves ORDER BY categorieEp")
+        except Exception as e:
+            self.ui.comboBox_fct_comp_3.clear()
+        else:
+            display.refreshGenericCombo(self.ui.comboBox_comp_3, result)
+
